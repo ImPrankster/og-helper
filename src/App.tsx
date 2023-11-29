43011d1d59a9b8e01215f37c179e8dca2e8eed9b
Ruofan Wong <meimprankster@gmail.com>
2023-11-29 18:50:18 +0800

    add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useQuery } from "@tanstack/react-query";
+import ogs from "open-graph-scraper-lite";
+
+async function getLocalHost() {
+  const html = await fetch("http://localhost:5173/").then((res) => res.text());
+  const res = await ogs({ html: html });
+  return res;
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { data, isLoading, isSuccess } = useQuery({
+    queryKey: ["lh"],
+    queryFn: getLocalHost,
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="flex flex-col min-h-screen w-screen">
+      {!(isLoading && isSuccess) && (
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">{data && JSON.stringify(data)}</h2>
+          </div>
+        </div>
+      )}
+    </main>
   );
 }
 
